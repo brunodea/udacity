75629914ad4b3f1f974c001046f8d72476d11b8e
Bruno Romero de Azevedo <bruno.azevedo@lge.com>
2017-08-29 11:40:04 -0300

    Add empty TheMovieDBAdapter class to be used in a RecyclerView.

---
+++ PopMovies/app/src/main/java/brunodea/udacity/com/popmovies/adapter/TheMovieDBAdapter.java
@@ -0,0 +1,37 @@
+package brunodea.udacity.com.popmovies.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Adapter class to be used by some RecyclerView.
+ * It takes data from TheMovieDB and populate a ViewHolder with the gathered information.
+ */
+public class TheMovieDBAdapter extends RecyclerView.Adapter<TheMovieDBAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder() {
+            //TODO
+            super(null);
+        }
+    }
+
+    public TheMovieDBAdapter() {
+        //TODO
+    }
+
+    @Override
+    public TheMovieDBAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // TODO
+        return null;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        //TODO
+    }
+
+    @Override
+    public int getItemCount() {
+        //TODO
+        return 0;
+    }
+}
