5aea98711d67d4e088fd4a243580624cdd832f05
Bruno Romero de Azevedo <bruno.azevedo@lge.com>
2017-08-29 11:39:27 -0300

    Add support for RecyclerView.

--- PopMovies/app/src/main/java/brunodea/udacity/com/popmovies/MainActivity.java
+++ PopMovies/app/src/main/java/brunodea/udacity/com/popmovies/MainActivity.java
@@ -1,9 +1,12 @@
 package brunodea.udacity.com.popmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRVPopMoviesh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
