920755feab5c3870c12fb35cf17dabd6f55e33c2
Bruno Romero de Azevedo <bruno.azevedo@lge.com>
2017-11-28 14:32:45 -0200

    Add UdaBake project

---
+++ UdaBake/app/src/main/java/udabake/brunodea/com/udabake/MainActivity.java
@@ -0,0 +1,13 @@
+package udabake.brunodea.com.udabake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
